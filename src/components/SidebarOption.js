bfae411a04c766f5d4236e49e4011261f43bc6bb
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-08-14 18:10:20 +0700

    ^ sidebar

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -2,7 +2,12 @@ import React from 'react';
 import './SidebarOption.css';
 
 const SidebarOption = ({ title, Icon }) => {
-    return <div className="sidebarOption">{title}</div>;
+    return (
+        <div className="sidebarOption">
+            {Icon && <Icon className="sidebarOption__icon" />}
+            {Icon ? <h4>{title}</h4> : <p>{title}</p>}
+        </div>
+    );
 };
 
 export default SidebarOption;
