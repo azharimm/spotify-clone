4e5d162318ce734152164691ab96f80f713d25e0
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-08-14 07:45:13 +0700

    + spotify helper func

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Login from './pages/Login';
+import { getTokeFromUrl } from './spotify';
 import './App.css';
 
 function App() {
+    const [token, setToken] = useState(null);
+
+    useEffect(() => {
+        const hash = getTokeFromUrl();
+        window.location.hash = '';
+        const _token = hash.access_token;
+        if (_token) {
+            setToken(_token);
+        }
+    }, []);
     return (
-        <div className="App">
-            <h1>Spotify Clone</h1>
-        </div>
+        <div className="App">{token ? <h1>I am logged In</h1> : <Login />}</div>
     );
 }
 
