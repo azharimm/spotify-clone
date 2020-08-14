674a0395dba1ff2ef9e1238720e7d068a3a980cc
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-08-14 19:38:07 +0700

    ^ footer style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,50 @@
-import React from 'react'
-import './Footer.css'
+import React from 'react';
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+import ShuffleIcon from '@material-ui/icons/Shuffle';
+import RepeatIcon from '@material-ui/icons/Repeat';
+import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay';
+import VolumeDownIcon from '@material-ui/icons/VolumeDown';
+import { Grid, Slider } from '@material-ui/core';
+import './Footer.css';
 
 const Footer = () => {
     return (
         <div className="footer">
-            <h1>Footer Here</h1>
+            <div className="footer__left">
+                <img
+                    className="footer__albumLogo"
+                    src="https://i.pinimg.com/236x/11/2e/3b/112e3b9716252fe63dd89893ba57d4ed.jpg"
+                    alt=""
+                />
+                <div className="footer__songInfo">
+                    <h4>Yeah!</h4>
+                    <p>Kangen Band</p>
+                </div>
+            </div>
+            <div className="footer__center">
+                <ShuffleIcon className="footer__green" />
+                <SkipPreviousIcon className="footer__icon" />
+                <PlayCircleOutlineIcon className="footer__icon" fontSize="large" />
+                <SkipNextIcon className="footer__icon" />
+                <RepeatIcon className="footer__green" />
+            </div>
+            <div className="footer__right">
+                <Grid container spacing={2}>
+                    <Grid item>
+                        <PlaylistPlayIcon />
+                    </Grid>
+                    <Grid item>
+                        <VolumeDownIcon />
+                    </Grid>
+                    <Grid item xs>
+                        <Slider />
+                    </Grid>
+                </Grid>
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default Footer
+export default Footer;
